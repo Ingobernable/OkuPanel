4c2187497adbe5d992a6183ee04ffca9a62fb974
moon155 <luna155@riseup.net>
2018-05-27 15:36:00 +0200

    fixed global ics output

--- okupanel/templates/ics.php
+++ okupanel/templates/ics.php
@@ -9,17 +9,27 @@ if (!empty($_GET['okupanel_debug']) && current_user_can('manage_options')){
 	error_reporting(E_ALL);
 }
 
-show_admin_bar(false);
+require OKUPANEL_PATH.'/inc/lib/SimpleICS.php';
 
-header('Content-type: text/calendar; charset=utf-8');
-header('Content-Disposition: attachment; filename=' . $filename);
 
-?><?php
-	# if (!($events = okupanel_get_events()))
-	# 	return '';
-	# print_r($events);
-	$base = WP_CONTENT_DIR.'/cache';
-	$cached_ics = $base.'/okupanel-cache.ics';
-	if (!($file = file_get_contents($cached_ics, FILE_USE_INCLUDE_PATH)))
-		return '';
-	print($file);
+$cal = new SimpleICS();
+
+global $okupanel_ics_ev;
+foreach (okupanel_get_events() as $ev){
+	$okupanel_ics_ev = $ev;
+	$cal->addEvent(function($e){
+		global $okupanel_ics_ev;
+		$e->startDate = new DateTime(date('Y-m-d H:i:s', $okupanel_ics_ev['start_gmt']));
+		$e->endDate = new DateTime(date('Y-m-d H:i:s', $okupanel_ics_ev['end_gmt']));
+		$e->uri = $okupanel_ics_ev['htmlLink'];
+		$e->location = $okupanel_ics_ev['location'];
+		$e->description = $okupanel_ics_ev['description'];
+		$e->summary = $okupanel_ics_ev['summary'];
+	});
+}
+
+
+header('Content-Type: '.SimpleICS::MIME_TYPE);
+header('Content-Disposition: attachment; filename=calendar.ics');
+echo $cal->serialize();
+exit();
